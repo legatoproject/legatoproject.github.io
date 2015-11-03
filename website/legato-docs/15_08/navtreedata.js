0568c623adb7b4e569c6de3e8ecaa957dfcc1698
tabenoza <tabenoza@sierrawireless.com>
2015-11-03 15:27:19 +0100

    website

---
+++ website/legato-docs/15_08/navtreedata.js
@@ -0,0 +1,26 @@
+var NAVTREE =
+[
+  [ "LEGATO", "index.html", [
+    [ "Reference", "index.html", "index" ],
+    [ "File List", "files.html", "files" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"about_docs.html",
+"c_le_voicecall.html#c_le_voicecall_incoming",
+"ccoding_stds_param.html",
+"le__adc__interface_8h.html",
+"le__avdata__interface_8h.html#a45fab2cd681a4e0c20b4e0a4fd97aa9a",
+"le__ecall__interface_8h.html#a777e8d6608b5e40f73696904471519cfa37adae3ab192525db0848a626869f9bf",
+"le__info__interface_8h.html#ae4a7baef2aac776edefefab68b74a7c6",
+"le__mdc__interface_8h.html#ad1fe8909ae01a2f7e33fdbdc6938bf17afeaf658e0acdb7413caef8fee314cfbb",
+"le__mrc__interface_8h.html#acb434a9f91afb5b6639baf3678ee9388",
+"le__sms__interface_8h.html",
+"le__sup__wdog__interface_8h_source.html",
+"tools_target_cm.html#toolsTarget_cm_data"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ website/legato-docs/15_08/searchdata.js
@@ -0,0 +1,42 @@
+var indexSectionsWithContent =
+{
+  0: "_abcdefghilmnoprstuvwy",
+  1: "l",
+  2: "al",
+  3: "_al",
+  4: "hmnpstu",
+  5: "l",
+  6: "al",
+  7: "l",
+  8: "_acilns",
+  9: "abcdefghilmnoprstuvwy"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "enums",
+  7: "enumvalues",
+  8: "defines",
+  9: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Enumerations",
+  7: "Enumerator",
+  8: "Macros",
+  9: "Pages"
+};
+
