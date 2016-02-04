8f58728413d00ab04a43dec81d7cf247717d4f86
Dmitry Narkevich <dnarkevich@sierrawireless.com>
2016-02-04 10:43:41 -0800

    starting point for page change

--- website/TOOLS/gen.py
+++ website/TOOLS/gen.py
@@ -136,8 +136,7 @@ def split_toc_into_categories(filepath):
 def gen_autotopmenu(dir, filename, contents):
     innerHTML = '<a href="{0}"{1}>{2}</a>'
     outerHTML = """
-    <div id="topMenu">
-    <a href=/docs/ class="biglink">Legato Docs</a>
+    <div id="topMenu" class="orange">
     <nav>
     {0}
     </nav>
