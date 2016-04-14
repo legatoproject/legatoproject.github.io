c3da46c84e019e3480184f778a5c468b864879fa
Dmitry Narkevich <dnarkevich@sierrawireless.com>
2016-04-14 12:40:46 -0700

    new landing page draft

---
+++ resources/js/particles.js
@@ -0,0 +1,9 @@
+    document.addEventListener('DOMContentLoaded', function() {
+        particleground(document.getElementById('particles'), {
+            parallax: true,
+            parallaxMultiplier: 20,
+            dotColor: '#A9C3CC',
+            lineColor: '#A9C3CC',
+            density: 5000
+        });
+    }, false);
