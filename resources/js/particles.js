6eed48728d1105b2379e4504e834a01a1f5c5f70
Dmitry Narkevich <dnarkevich@sierrawireless.com>
2016-04-25 15:54:52 -0700

    further work

--- resources/js/particles.js
+++ resources/js/particles.js
@@ -1,9 +1,14 @@
-    document.addEventListener('DOMContentLoaded', function() {
-        particleground(document.getElementById('particles'), {
-            parallax: true,
-            parallaxMultiplier: 20,
-            dotColor: '#A9C3CC',
-            lineColor: '#A9C3CC',
-            density: 5000
-        });
-    }, false);
+document.addEventListener('DOMContentLoaded', function() {
+    particleground(document.getElementById('particles'), {
+        parallax: true,
+        parallaxMultiplier: 40,
+        dotColor: '#B7CDD4',
+        lineColor: '#A9C3CC',
+        density: 4500,
+        minSpeedX: 0.1,
+	    maxSpeedX: 0.5,
+	    minSpeedY: 0.1,
+	    maxSpeedY: 0.5,
+
+    });
+}, false);
